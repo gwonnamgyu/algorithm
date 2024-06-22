a617c6a844321a053e321a018fffa98e7461aeed
gwonnamgyu <162396013+gwonnamgyu@users.noreply.github.com>
2024-06-22 21:42:58 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int A, B;
+    std::cin >> A >> B;
+
+    cout << A + B << endl; 
+    cout << A - B << endl; 
+    cout << A * B << endl; 
+    cout << A / B << endl; 
+    cout << A % B << endl; 
+
+    return 0;
+}
