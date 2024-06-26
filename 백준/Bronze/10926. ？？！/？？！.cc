f8a5423fb0d6b4f7eff4cd43a64d2386ca97aec2
gwonnamgyu <162396013+gwonnamgyu@users.noreply.github.com>
2024-06-26 14:16:12 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string user_id;
+    cin >> user_id;
+
+    string surprise = user_id + "??!";
+
+    cout << surprise << endl;
+
+    return 0;
+}
